e591827422f69a33500b822336e6bc35234ace19
Gustavo Paulo <gustavo.paulo.segura@gmail.com>
2021-05-12 15:49:17 -0400

    fixing setup.js

--- db/setup.js
+++ db/setup.js
@@ -12,57 +12,78 @@ const { Episode, Location, Character } = db;
   await db.sequelize.sync({ force: true });
 
   // seed data
-  try {
-    const response = await axios.get("https://rickandmortyapi.com/api/location");
-    await Location.bulkCreate(response.data.results);
-    console.log("seed: locations saved");
-  } catch (err) {
-    console.error(err);
-    console.error("no se pudo cargar las ubicaciones desde la API");
-    process.exit(1);
+  let page = 1;
+  let count = 1;
+  while(page <= count) {
+    try {
+      const response = await axios.get(`https://rickandmortyapi.com/api/location?page=${page}`);
+      count = response.data.info.pages;
+
+      await Location.bulkCreate(response.data.results);
+      console.log(`seed: locations saved (page ${page})`);
+      page += 1;
+    } catch (err) {
+      console.error(err);
+      console.error("no se pudo cargar las ubicaciones desde la API");
+      process.exit(1);
+    }
   }
 
-  try {
-    const response = await axios.get("https://rickandmortyapi.com/api/episode");
-    await Episode.bulkCreate(response.data.results);
-    console.log("seed: episodes saved");
-  } catch (err) {
-    console.error(err);
-    console.error("no se pudo cargar los episodios desde la API");
-    process.exit(1);
+  page = 1;
+  count = 1;
+  while(page <= count) {
+    try {
+      const response = await axios.get(`https://rickandmortyapi.com/api/episode?page=${page}`);
+      count = response.data.info.pages;
+
+      await Episode.bulkCreate(response.data.results);
+      console.log(`seed: episodes saved (page ${page})`);
+      page += 1;
+    } catch (err) {
+      console.error(err);
+      console.error("no se pudo cargar los episodios desde la API");
+      process.exit(1);
+    }
   }
 
-  try {
-    const response = await axios.get("https://rickandmortyapi.com/api/character");
-    const prom = response.data.results.map((item) => {
-      return (async () => {
-        const obj = Character.build(item);
+  page = 1;
+  count = 1;
+  while(page <= count) {
+    try {
+      const response = await axios.get(`https://rickandmortyapi.com/api/character?page=${page}`);
+      count = response.data.info.pages;
 
-        // check origin
-        const origin = await Location.findOne({ where: { name: item.origin.name } });
-        if (origin) obj.origin_id = origin.id;
+      const prom = response.data.results.map((item) => {
+        return (async () => {
+          const obj = Character.build(item);
 
-        // check location
-        const location = await Location.findOne({ where: { name: item.location.name } });
-        if (location) obj.location_id = location.id;
+          // check origin
+          const origin = await Location.findOne({ where: { name: item.origin.name } });
+          if (origin) obj.origin_id = origin.id;
 
-        // save
-        const res = await obj.save();
+          // check location
+          const location = await Location.findOne({ where: { name: item.location.name } });
+          if (location) obj.location_id = location.id;
 
-        // add episodes
-        const episodes = await Episode.findAll({
-          where: { url: { [Op.in]: item.episode } },
-        });
-        await obj.setEpisode(episodes);
-      })();
-    });
+          // save
+          const res = await obj.save();
 
-    // await all
-    await Promise.all(prom);
-    console.log("seed: characters saved");
-  } catch (err) {
-    console.error(err);
-    console.error("no se pudo cargar los caracteres desde la API");
-    process.exit(1);
+          // add episodes
+          const episodes = await Episode.findAll({
+            where: { url: { [Op.in]: item.episode } },
+          });
+          await obj.setEpisode(episodes);
+        })();
+      });
+
+      // await all
+      await Promise.all(prom);
+      console.log(`seed: characters saved (page ${page})`);
+      page += 1;
+    } catch (err) {
+      console.error(err);
+      console.error("no se pudo cargar los caracteres desde la API");
+      process.exit(1);
+    }
   }
 })();
